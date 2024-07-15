b5dd8211a1db35cc48194fb81560a07f2bcddbab
lana-12 <lana.12@wanadoo.fr>
2024-07-15 16:41:20 +0200

    Commit tp9

--- src/fr/utils/TestMethodStatic.java
+++ src/fr/utils/TestMethodStatic.java
@@ -5,18 +5,24 @@ public class TestMethodStatic {
     public static void main(String[] args) {
 
         String chaine ="12";
-        int result = Integer.parseInt(chaine);
-
-        System.out.println(result);
-        System.out.println(chaine);
-
+        caster(chaine);
 
         int a = 20;
         int b = 10;
+        trouverMax(a,b);
+
+    }
+
+    public static int caster(String chaine) {
+        int result = Integer.parseInt(chaine);
+        System.out.println(result);
+        return result;
+    }
+
+    public static int trouverMax(int a, int b) {
         int max = Integer.max(a, b);
         System.out.println("Le nombre maximum entre " + a + " et " + b + " est : " + max);
-
-
+        return max;
     }
 
 
